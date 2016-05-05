81245a1bfa24939bf2cdfdd61ec1531b28dd8d90
Marius Schär <schaer.marius@gmail.com>
2016-05-05 21:28:02 +0200

    added some basic views

---
+++ App/Views/head.view.php
@@ -0,0 +1,9 @@
+<head>
+    <style type="text/css">
+        html, body{
+            font-family: SansSerif;
+            color: #000000;
+        }
+    </style>
+    <title>imagic</title>
+</head>
---
+++ App/Views/home.view.php
@@ -0,0 +1,19 @@
+<?php
+use \Views\Renderer;
+
+?>
+<html>
+<?php Renderer::view('head') ?>
+<body>
+<h1>imagic</h1>
+<span>welcome to imagic. the simple image hosting service.</span>
+<h2>features</h2>
+<span>
+    <ul>
+        <li>galleries</li>
+        <li>thumbnails</li>
+        <li>private pictures & galleries</li>
+    </ul>
+</span>
+</body>
+</html>
