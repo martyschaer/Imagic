4c491cc4e5c94abe3d6f9cda96a604916068efbf
Marius Schär <schaer.marius@gmail.com>
2016-05-06 23:45:44 +0200

    added security helper

--- App/Utilities/Constants.php
+++ App/Utilities/Constants.php
@@ -10,4 +10,10 @@ class Constants
 {
     const DB_USER = 'imagic';
     const DB_PASS = 'ZfvXN2JnHeP56F73';
+
+    //this amount will roughly take 1 second per password on my machine,
+    //which is ideal according to the linked question.
+    //http://security.stackexchange.com/questions/3959/recommended-of-iterations-when-using-pkbdf2-sha256/3993#3993
+    //this is given as a power of 2, so 2^19 = 524288
+    const SEC_BCRYPT_ROUNDS = 14;
 }
