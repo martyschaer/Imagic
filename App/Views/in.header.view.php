d725bfe7841ffe70725a64f6f7063fdbe1004ff4
Marius Schär <schaer.marius@gmail.com>
2016-05-07 18:58:36 +0200

    added some basic css

--- App/Views/in.header.view.php
+++ App/Views/in.header.view.php
@@ -1,7 +1,7 @@
 <header>
-    <h1><a href="/">imagic</a></h1>
+    <a href="/"><img id="logo" src="{{RESOURCE_PATH}}/icons/logo.png"></a>
     <ul>
-        <li>{{USER_EMAIL}}</li>
-        <li><a href="/user/logout">logout</a></li>
+        <li class="user_email">{{USER_EMAIL}}</li>
+        <li><a class="light" href="/user/logout">logout</a></li>
     </ul>
 </header>
--- App/Views/out.header.view.php
+++ App/Views/out.header.view.php
@@ -1,7 +1,7 @@
 <header>
-    <h1><a href="/">imagic</a></h1>
+    <a href="/"><img id="logo" src="{{RESOURCE_PATH}}/icons/logo.png"></a>
     <ul>
-        <li><a href="/register">register</a></li>
-        <li><a href="/login">login</a></li>
+        <li><a class="light" href="/register">register</a></li>
+        <li><a class="light" href="/login">login</a></li>
     </ul>
 </header>
